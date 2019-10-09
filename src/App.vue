b91c136acb4581194e3010ccc852bbdd9a723bad
chapkovski <chapkovski@gmail.com>
2019-10-09 17:54:59 -0400

    upd

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,31 @@
         header-bg-variant="primary"
         border-variant="primary"
       >
-        <reallocation :allocations="realloc_choices" v-model="chosen_reallocation"></reallocation>
-        <tax-rates :rates="tax_rates" v-model="chosen_tax_rate"></tax-rates>
-        <result :tax="chosen_tax_rate" :chosen_paramset="chosen_paramset" v-if="chosen_paramset && chosen_tax_rate!=undefined"></result>
+        <b-row>
+          <b-col cols="4">
+            <reallocation :allocations="realloc_choices" v-model="chosen_reallocation"></reallocation>
+            <tax-rates
+              :rates="tax_rates"
+              v-model="chosen_tax_rate"
+              v-if="chosen_reallocation!=undefined"
+            ></tax-rates>
+          </b-col>
+          <b-col cols="4" align-h="center">
+            <transition name="fade">
+              <b-alert :show="chosen_reallocation===undefined">Please choose reallocation option</b-alert>
+            </transition>
+            <b-alert
+              variant="danger"
+              :show="chosen_reallocation!=undefined && chosen_tax_rate===undefined "
+            >Now choose the tax rate</b-alert>
+
+            <result
+              :tax="chosen_tax_rate"
+              :chosen_paramset="chosen_paramset"
+              v-if="chosen_paramset && chosen_tax_rate!=undefined"
+            ></result>
+          </b-col>
+        </b-row>
       </b-card>
     </b-container>
   </div>
@@ -33,8 +55,9 @@ export default {
   },
   data() {
     return {
+      show: true,
       realloc_choices: [0, 1],
-      tax_rates: [0,.3,.4348],
+      tax_rates: [0, 0.3, 0.4348],
       chosen_tax_rate: undefined,
       chosen_reallocation: undefined,
       params: {
@@ -42,24 +65,24 @@ export default {
           H: {
             capacity: 1,
             productivity: 1,
-            number:2
+            number: 2
           },
           L: {
             capacity: 1,
             productivity: 1,
-            number:2
+            number: 2
           }
         },
         1: {
           H: {
             capacity: 1,
             productivity: 4.6,
-            number:2
+            number: 2
           },
           L: {
             capacity: 0,
             productivity: 0,
-            number:2
+            number: 2
           }
         }
       }
@@ -67,7 +90,7 @@ export default {
   },
   computed: {
     chosen_paramset: function() {
-      return this.params[this.chosen_reallocation]
+      return this.params[this.chosen_reallocation];
     }
   }
 };
@@ -81,5 +104,12 @@ export default {
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
